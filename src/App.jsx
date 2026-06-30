9d49e34ace41432a0378e1b729938ba8c4786a49
Kiran <kiran.student.ece24@itbhu.ac.in>
2026-06-30 22:11:37 +0530

    fix ReportIssue import case

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-import ReportIssue from "./pages/ReportIssue";
+import ReportIssue from "./pages/reportissue";
 
 function App() {
   return (
